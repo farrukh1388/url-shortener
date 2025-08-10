1dc68838d84afb1aae1712d0fea285b0c5fdfd32
18540314 <FKKurbanbaev@sberbank.ru>
2025-08-10 17:31:19 +0200

    Initialized empty Spring Boot web app

---
+++ src/test/kotlin/de/dkbcf/urlshortener/UrlShortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.dkbcf.urlshortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
